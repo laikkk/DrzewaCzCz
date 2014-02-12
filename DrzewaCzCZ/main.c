d2b8981bdde1d3fd66deeff5a782103af8f428f4
Kamil Zieliński <kamil@kamil-VirtualBox>
2014-02-12 12:20:13 +0100

    Start Project

---
+++ DrzewaCzCZ/main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: kamil
+ *
+ * Created on 12 luty 2014, 12:19
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return (EXIT_SUCCESS);
+}
+
